fe22bc81071a50db3641abaa047525fa817b12be
Gabe Odess <gabeodess@gmail.com>
2017-01-24 16:39:04 -0500

    changed lambda syntax

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -17,10 +17,10 @@ class Contribution < ActiveRecord::Base
   # ==========
   # = Scopes =
   # ==========
-  scope :find_by_year, (lambda do |year|
+  scope :find_by_year, lambda{ |year|
     start = Date.parse("01-01-#{year}")
     where({:date => start..start.end_of_year})
-  end)
+  }
   scope :find_emailable, ->{ joins(:contributer_email_address) }
 
   # ================
