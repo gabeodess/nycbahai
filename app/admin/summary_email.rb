69d27a079a6ae3befdacb8a14f90e2381229e349
Gabe Odess <gabeodess@gmail.com>
2017-02-17 20:01:53 -0500

    fixed summary emails

--- app/admin/summary_email.rb
+++ app/admin/summary_email.rb
@@ -4,4 +4,15 @@ ActiveAdmin.register SummaryEmail do
     link_to "Send Summaries for #{SummaryEmail.default_year}", :send_summaries_admin_contributions, :method => :post, :data => {:disable_with => '...'}
   end
 
+  permit_params :contributer, :year
+
+  form do |f|
+    f.object.year ||= Time.zone.today.year - 1
+    f.inputs do
+      f.input :contributer, collection: Contribution.reorder(:name).pluck("DISTINCT name")
+      f.input :year
+    end
+    f.actions
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
   # Settings specified here will take precedence over those in config/application.rb.
 
